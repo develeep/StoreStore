28df0157777a2159bca49eb061959512c09b288c
daun3046 <daun3046@swu.ac.kr>
2022-05-25 02:29:27 +0900

    add userinfo

--- src/views/userinfo/userinfo.js
+++ src/views/userinfo/userinfo.js
@@ -1,11 +1,3 @@
-회원정보 관리
-논의할 사항
-비밀번호 양식 통일 (ex. 8글자 이상, 영문숫자 혼용 등...)
-BackEnd 요청
-처음 실행되었을 때:
-1. `이름`과 `주소 값`을 저장한 적 있으면 => DB에서 불러옴
-2. 없으면: 회원가입시 입력했던 `이름`만 DB에서 불러옴
-
 // 요소 가져오기
 const userInfoTitle = document.querySelector('#userInfoTitle')
 const nameInput = document.querySelector('#nameInput');
