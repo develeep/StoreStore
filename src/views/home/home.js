fad1929065475b1826ec3fbc243dbbaaf44d208f
[SW2/서울/2반] 연은빈 <okdol0611@gmail.com>
2022-05-25 16:33:12 +0900

    회원탈퇴 페이지 구현

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,20 +1,17 @@
 // 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
 // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // 코드 예시를 남겨 두었습니다.
-import { loginMatch } from '/loginMatch.js';
-import * as Api from '/api.js';
-import { randomId} from '/useful-functions.js';
+import { loginMatch } from "/loginMatch.js";
+import * as Api from "/api.js";
+import { randomId } from "/useful-functions.js";
 
 // 요소(element), input 혹은 상수
-const landingDiv = document.querySelector('#landingDiv');
-const greetingDiv = document.querySelector('#greetingDiv');
+const landingDiv = document.querySelector("#landingDiv");
+const greetingDiv = document.querySelector("#greetingDiv");
 
-console.log(loginMatch)
+console.log(loginMatch);
 addAllElements();
 addAllEvents();
-
-
-
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 async function addAllElements() {
@@ -25,13 +22,13 @@ async function addAllElements() {
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  landingDiv.addEventListener('click', alertLandingText);
-  greetingDiv.addEventListener('click', alertGreetingText);
+  landingDiv.addEventListener("click", alertLandingText);
+  greetingDiv.addEventListener("click", alertGreetingText);
 }
 
 function insertTextToLanding() {
   landingDiv.insertAdjacentHTML(
-    'beforeend',
+    "beforeend",
     `
       <h2>n팀 쇼핑몰의 랜딩 페이지입니다. 자바스크립트 파일에서 삽입되었습니다.</h2>
     `
@@ -40,7 +37,7 @@ function insertTextToLanding() {
 
 function insertTextToGreeting() {
   greetingDiv.insertAdjacentHTML(
-    'beforeend',
+    "beforeend",
     `
       <h1>반갑습니다! 자바스크립트 파일에서 삽입되었습니다.</h1>
     `
@@ -48,16 +45,16 @@ function insertTextToGreeting() {
 }
 
 function alertLandingText() {
-  alert('n팀 쇼핑몰입니다. 안녕하세요.');
+  alert("n팀 쇼핑몰입니다. 안녕하세요.");
 }
 
 function alertGreetingText() {
-  alert('n팀 쇼핑몰에 오신 것을 환영합니다');
+  alert("n팀 쇼핑몰에 오신 것을 환영합니다");
 }
 
 async function getDataFromApi() {
   // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
-  const data = await Api.get('/api/user/data');
+  const data = await Api.get("/api/user/data");
   const random = randomId();
 
   console.log({ data });
