346d9ff0a26bbf13c7f566e0d60a3090dd363630
Lee <dltmdgns0508@gmail.com>
2022-05-25 12:08:39 +0900

    후에 다른 화면에서도 로그인에 따른 navbar 차이를 주기 위해 loginMatch 함수를 따로 파일로 뺌.

---
+++ src/views/loginMatch.js
@@ -0,0 +1,28 @@
+
+export const loginMatch = () => {
+  const navBar = document.querySelector('#navbar');
+  const token = localStorage.getItem('token');
+  if (token) {
+    const logout = createA('/', '로그아웃');
+    const myPage = createA('/userInfo', '마이페이지');
+    logout.addEventListener('click', (e) => {
+      e.preventDefault();
+      localStorage.removeItem('token');
+      location.reload();
+    });
+    navBar.prepend(logout, myPage);
+  }
+  else {
+    const login = createA('/login', '로그인');
+    const register = createA('/register', '회원가입');
+    navBar.prepend(login, register);
+  }
+};
+function createA(href, text) {
+  const liTag = document.createElement('li');
+  const aTag = document.createElement('a');
+  aTag.href = href;
+  aTag.textContent = text;
+  liTag.append(aTag);
+  return liTag;
+}
