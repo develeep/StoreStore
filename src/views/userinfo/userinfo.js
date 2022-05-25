12d287edafd64d3529ac441dae724efa012387f9
daun3046 <daun3046@swu.ac.kr>
2022-05-25 22:25:08 +0900

    conflict 해결중

--- src/views/userinfo/userinfo.js
+++ src/views/userinfo/userinfo.js
@@ -12,6 +12,10 @@ const extraAddressDiv = document.querySelector('#sample6_extraAddress');
 const phoneNumberInput = document.querySelector('#phoneNumberInput');
 const saveButton = document.querySelector('#saveButton');
 
+saveButton.addEventListener('click', comparePassword);
+saveButton.addEventListener('click', confirmPhoneNumber);
+saveButton.addEventListener('click', addressConfirm);
+
 getUserInfo();
 
 async function getUserInfo() {
@@ -23,22 +27,23 @@ async function getUserInfo() {
 		alert(`회원정보를 받아오지 못했습니다.: ${err.message}`);
 	}
 }
+
 // 비밀번호와 비밀번호 확인의 값을 비교하는 함수
 function comparePassword(e) {
-	e.preventDefault();
-	if (passwordInput.value !== passwordConfirmInput.value) {
-		alert('입력된 비밀번호와 비밀번호 확인의 값이 다릅니다.');
-	}
+    e.preventDefault();
+    if (passwordInput.value !== passwordConfirmInput.value) {
+        alert('입력된 비밀번호와 비밀번호 확인의 값이 다릅니다.');
+    }
 }
 
 // 전화번호가 유효한지 확인하는 함수. -(하이픈) 유무에 상관없게 작성함.
 function confirmPhoneNumber(e) {
-	e.preventDefault();
-	if (/^[0-9]{2,3}-?[0-9]{3,4}-?[0-9]{4}/.test(phoneNumberInput.value)) {
-		return;
-	}
-	alert('유효하지 않은 전화번호입니다.');
-	return;
+    e.preventDefault();
+    if (/^[0-9]{2,3}-?[0-9]{3,4}-?[0-9]{4}/.test(phoneNumberInput.value)) {
+        return;
+    }
+    alert('유효하지 않은 전화번호입니다.')
+    return;
 }
 
 // 주소 전달과 확인용 함수. 콘솔에서 입력된 주소를 확인.
