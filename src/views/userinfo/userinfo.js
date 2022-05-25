4e258983991301b859c396399b36cf2797897088
daun3046 <daun3046@swu.ac.kr>
2022-05-25 21:53:07 +0900

    주소 api 추가, 주소 데이터 확인

--- src/views/userinfo/userinfo.js
+++ src/views/userinfo/userinfo.js
@@ -5,10 +5,10 @@ const userInfoTitle = document.querySelector('#userInfoTitle');
 const nameInput = document.querySelector('#nameInput');
 const passwordInput = document.querySelector('#passwordInput');
 const passwordConfirmInput = document.querySelector('#passwordConfirmInput');
-const postalCodeInput = document.querySelector('#postalCodeInput');
-const searchAddressButton = document.querySelector('#searchAddressButton');
-const addressInput = document.querySelector('#addressInput');
-const address2Input = document.querySelector('#address2Input');
+const postalCodeDiv = document.querySelector('#sample6_postcode');
+const addressDiv = document.querySelector('#sample6_address');
+const detailAddressDiv = document.querySelector('#sample6_detailAddress');
+const extraAddressDiv = document.querySelector('#sample6_extraAddress');
 const phoneNumberInput = document.querySelector('#phoneNumberInput');
 const saveButton = document.querySelector('#saveButton');
 getUserInfo();
@@ -22,14 +22,16 @@ async function getUserInfo() {
 	}
 }
 // 비밀번호와 비밀번호 확인의 값을 비교하는 함수
-function comparePassword() {
+function comparePassword(e) {
+	e.preventDefault();
 	if (passwordInput.value !== passwordConfirmInput.value) {
 		alert('입력된 비밀번호와 비밀번호 확인의 값이 다릅니다.');
 	}
 }
 
 // 전화번호가 유효한지 확인하는 함수. -(하이픈) 유무에 상관없게 작성함.
-function confirmPhoneNumber() {
+function confirmPhoneNumber(e) {
+	e.preventDefault();
 	if (/^[0-9]{2,3}-?[0-9]{3,4}-?[0-9]{4}/.test(phoneNumberInput.value)) {
 		return;
 	}
@@ -37,5 +39,25 @@ function confirmPhoneNumber() {
 	return;
 }
 
+// 주소 전달과 확인용 함수. 콘솔에서 입력된 주소를 확인.
+function addressConfirm(e) {
+    e.preventDefault();
+
+    const postalCode = postalCodeDiv.value;
+    const address1 = addressDiv.value + extraAddressDiv.value;
+    const address2 = detailAddressDiv.value;
+    // 저장될 주소 객체
+    const address = {
+        postalCode,
+        address1,
+        address2,
+    }
+
+    // 콘솔 주소 확인용
+    const str = JSON.stringify(address);
+    console.log(str);
+}
+
 saveButton.addEventListener('click', comparePassword);
 saveButton.addEventListener('click', confirmPhoneNumber);
+saveButton.addEventListener('click', addressConfirm);
