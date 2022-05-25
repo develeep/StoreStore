f48191bee2bac0805bd32987dc58e6d292f59d66
Lee <dltmdgns0508@gmail.com>
2022-05-25 18:03:09 +0900

    prettier 형식으로 파일 변경

--- src/views/userinfo/userinfo.js
+++ src/views/userinfo/userinfo.js
@@ -1,41 +1,41 @@
 // 요소 가져오기
-import * as Api from "/api.js";
+import * as Api from '/api.js';
 
-const userInfoTitle = document.querySelector("#userInfoTitle");
-const nameInput = document.querySelector("#nameInput");
-const passwordInput = document.querySelector("#passwordInput");
-const passwordConfirmInput = document.querySelector("#passwordConfirmInput");
-const postalCodeInput = document.querySelector("#postalCodeInput");
-const searchAddressButton = document.querySelector("#searchAddressButton");
-const addressInput = document.querySelector("#addressInput");
-const address2Input = document.querySelector("#address2Input");
-const phoneNumberInput = document.querySelector("#phoneNumberInput");
-const saveButton = document.querySelector("#saveButton");
+const userInfoTitle = document.querySelector('#userInfoTitle');
+const nameInput = document.querySelector('#nameInput');
+const passwordInput = document.querySelector('#passwordInput');
+const passwordConfirmInput = document.querySelector('#passwordConfirmInput');
+const postalCodeInput = document.querySelector('#postalCodeInput');
+const searchAddressButton = document.querySelector('#searchAddressButton');
+const addressInput = document.querySelector('#addressInput');
+const address2Input = document.querySelector('#address2Input');
+const phoneNumberInput = document.querySelector('#phoneNumberInput');
+const saveButton = document.querySelector('#saveButton');
 getUserInfo();
 async function getUserInfo() {
-  try {
-    const userData = await Api.get("/api/update");
-    nameInput.value = userData.fullName;
-  } catch (err) {
-    console.error(err.stack);
-    alert(`회원정보를 받아오지 못했습니다.: ${err.message}`);
-  }
+	try {
+		const userData = await Api.get('/api/update');
+		nameInput.value = userData.fullName;
+	} catch (err) {
+		console.error(err.stack);
+		alert(`회원정보를 받아오지 못했습니다.: ${err.message}`);
+	}
 }
 // 비밀번호와 비밀번호 확인의 값을 비교하는 함수
 function comparePassword() {
-  if (passwordInput.value !== passwordConfirmInput.value) {
-    alert("입력된 비밀번호와 비밀번호 확인의 값이 다릅니다.");
-  }
+	if (passwordInput.value !== passwordConfirmInput.value) {
+		alert('입력된 비밀번호와 비밀번호 확인의 값이 다릅니다.');
+	}
 }
 
 // 전화번호가 유효한지 확인하는 함수. -(하이픈) 유무에 상관없게 작성함.
 function confirmPhoneNumber() {
-  if (/^[0-9]{2,3}-?[0-9]{3,4}-?[0-9]{4}/.test(phoneNumberInput.value)) {
-    return;
-  }
-  alert("유효하지 않은 전화번호입니다.");
-  return;
+	if (/^[0-9]{2,3}-?[0-9]{3,4}-?[0-9]{4}/.test(phoneNumberInput.value)) {
+		return;
+	}
+	alert('유효하지 않은 전화번호입니다.');
+	return;
 }
 
-saveButton.addEventListener("click", comparePassword);
-saveButton.addEventListener("click", confirmPhoneNumber);
+saveButton.addEventListener('click', comparePassword);
+saveButton.addEventListener('click', confirmPhoneNumber);
