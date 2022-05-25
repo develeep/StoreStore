5ef4e856516b4952929dcd383b7873e92afe556e
[SW2/서울/2반] 연은빈 <okdol0611@gmail.com>
2022-05-25 17:56:41 +0900

    카테고리 추가

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,77 +1,29 @@
-import * as Api from "/api.js";
-import { validateEmail } from "/useful-functions.js";
+// 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
+// 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
+// 코드 예시를 남겨 두었습니다.
+import { loginMatch } from '/loginMatch.js';
+import * as Api from '/api.js';
+import { randomId } from '/useful-functions.js';
 
-// 요소(element), input 혹은 상수
-const emailInput = document.querySelector("#emailInput");
-const passwordInput = document.querySelector("#passwordInput");
-const submitButton = document.querySelector("#submitButton");
+// // 요소(element), input 혹은 상수
 
+console.log(loginMatch);
 addAllElements();
 addAllEvents();
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {}
+async function addAllElements() {
+	loginMatch();
+}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-function addAllEvents() {
-  submitButton.addEventListener("click", handleSubmit);
+function addAllEvents() {}
+
+async function getDataFromApi() {
+	// 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
+	const data = await Api.get('/api/user/data');
+	const random = randomId();
+
+	console.log({ data });
+	console.log({ random });
 }
-
-// 로그인 진행
-async function handleSubmit(e) {
-  e.preventDefault();
-
-  const email = emailInput.value;
-  const password = passwordInput.value;
-
-  // 잘 입력했는지 확인
-  const isEmailValid = validateEmail(email);
-  const isPasswordValid = password.length >= 4;
-
-  if (!isEmailValid || !isPasswordValid) {
-    return alert(
-      "비밀번호가 4글자 이상인지, 이메일 형태가 맞는지 확인해 주세요."
-    );
-  }
-
-  // 로그인 api 요청
-  try {
-    const data = { email, password };
-
-    const result = await Api.post("/api/login", data);
-    const token = result.token;
-
-    // 로그인 성공, 토큰을 세션 스토리지에 저장
-    // 물론 다른 스토리지여도 됨
-    localStorage.setItem("token", token);
-
-    alert(`정상적으로 로그인되었습니다.`);
-
-    // 로그인 성공
-
-    // 기본 페이지로 이동
-    window.location.href = "/";
-  } catch (err) {
-    console.error(err.stack);
-    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
-  }
-}
-
-// 슬라이드 이미지
-new Swiper(".promotion .swiper-container", {
-  autoplay: {
-    delay: 5000, //5초
-  },
-  loop: true, // 반복 재생 여부
-  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
-  spaceBetween: 0, // 슬라이드 사이 여백
-  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
-  pagination: {
-    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
-    clickable: true, //사용자의 페이지 번호 요소 제어
-  },
-  navigation: {
-    prevEl: ".promotion .swiper-prev",
-    nextEl: ".promotion .swiper-next",
-  },
-});
