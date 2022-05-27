e63f51b3986b808a34dbf40075641af73621097f
OSEUNGJUN <tmdwns1674@gmail.com>
2022-05-27 23:41:13 +0900

    카테고리별 상품 보기

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -50,14 +50,4 @@ categoryRouter.get('/getcategorys', async (req, res, next) => {
 	}
 });
 
-categoryRouter.get('/getSmallcategory', async (req, res, next) => {
-	try {
-		// 위 데이터를 유저 db에 추가하기
-		let getSmallcategory = await SmallcateService.getCategoryname('운동화');
-		res.status(200).json(getSmallcategory);
-	} catch (error) {
-		next(error);
-	}
-});
-
 export { categoryRouter };
