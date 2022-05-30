745ce0ab835cff2bf60b686970489e0f5a6f3706
Lee <dltmdgns0508@gmail.com>
2022-05-30 23:43:07 +0900

    관리자 페이지 최초 설정

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -25,6 +25,8 @@ viewsRouter.use('/ranking', serveStatic('ranking'));
 viewsRouter.use('/category', serveStatic('category'));
 viewsRouter.use('/payment/:orderId',serveStatic('paymentComplete'))
 viewsRouter.use('/admin',serveStatic('admin'))
+viewsRouter.use('/admin/orderList', serveStatic('orderList'))
+viewsRouter.use('/admin/productAdd',serveStatic('productAdd'))
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
 
