8202ace43c35c8b5a09b9a0701c3d69f4567c9b2
Lee <dltmdgns0508@gmail.com>
2022-06-01 02:46:42 +0900

    카테고리 삽입,추가,수정,삭제 구현

--- src/views/productAdd/productAdd.js
+++ src/views/productAdd/productAdd.js
@@ -2,11 +2,12 @@ import { getElement, getElementAll } from '/useful-functions.js';
 import { renderGnb } from '/renderGnb.js';
 import * as Api from '/api.js';
 
-const categorySelectBox = document.getElementById('categorySelectBox');
-const categroyButton = document.getElementById('categroyButton');
-const BcategoryInput = document.getElementById('Bcategory');
-const ScategoryInput = document.getElementById('Scategory');
-const ScategoyUl = document.getElementById('ScategoyUl');
+const categorySelectBox = getElement('#categorySelectBox');
+const categroyButton = getElement('#categroyButton');
+const BcategoryInput = getElement('#Bcategory');
+const ScategoryInput = getElement('#Scategory');
+const ScategoyUl = getElement('#ScategoyUl');
+let categorysData = [];
 // const categorySelectBox = document.getElementById('categorySelectBox');
 
 addAllElements();
@@ -27,48 +28,32 @@ modalwrap.addEventListener('click', async (e) => {
 	document.querySelector('.modal_wrap').style.display = 'block';
 	document.querySelector('.black_bg').style.display = 'block';
 
+	categorySelectBox.innerHTML = null;
+
 	const categories = await Api.get('/api/getcategorys');
+
 	let categorys = Object.entries(categories);
-	categorySelectBox.innerHTML += `<option value>카테고리를 선택해주세요.</option>`;
-	for (const [key, value] of categorys) {
-		categorySelectBox.innerHTML += `<option value="${key}">${key}</option>`;
-	}
+	console.log(categories);
+	console.log(categorys);
+
+	const option = document.createElement('option');
+	option.textContent = '카테고리를 선택해 주세요.';
+	categorySelectBox.append(option);
+
+	categorys.forEach(([key, value]) => {
+		const optionItem = document.createElement('option');
+		optionItem.value = key;
+		optionItem.textContent = key;
+		categorySelectBox.append(optionItem);
+	});
+
 	categorySelectBox.addEventListener('change', async (event) => {
 		const targetCategory = event.target.value;
-		const categorysData = await categorys.find(
-			(e) => e[0] == targetCategory,
-		)[1];
-		console.log(categorysData);
-		await categoryReset(categorysData);
+		categorysData = categories[targetCategory];
+		// console.log(categorysData);
+		// await categoryReset(categorysData);
 		// 카테고리추가
-	});
-	const categoryAdd = document.getElementById('categoryAdd');
-	let cateogryInput = document.getElementById('cateogryInput');
-	categoryAdd.addEventListener('click', async (e) => {
-		e.preventDefault();
-		const ScateogryInput = cateogryInput.value;
-		console.log(ScateogryInput);
-		const data = { targetCategory, ScateogryInput };
-		const newCategory = await Api.post('/api/category_update', data);
-		if (newCategory.result == 'ok') {
-			categorysData.push(ScateogryInput);
-			await categoryReset(categorysData);
-		} else {
-			alert('error');
-		}
-	});
-	// 카테고리삭제
-	const categoryLi = document.querySelectorAll('#categoryId');
-	categoryLi.forEach(async (el) => {
-		const deleteButton = el.querySelector('#delete');
-		deleteButton.addEventListener('click', async (e) => {
-			e.preventDefault();
-			const selectedCategory = el.querySelector('#categoryName').innerText;
-			const data = { selectedCategory };
-			console.log(data);
-			const deleteCategory = await Api.delete('/api/Categorydelete', '', data);
-			console.log(deleteCategory);
-		});
+		categoryReset(targetCategory, categorysData);
 	});
 });
 
@@ -80,12 +65,123 @@ modalclose.addEventListener('click', async (e) => {
 	document.querySelector('.black_bg').style.display = 'none';
 });
 
-async function categoryReset(categorysData) {
-	let Scategorys = await categorysData.reduce(function (prev, curr) {
-		return (
-			prev +
-			`<li id = "categoryId"><button id = "categoryName">${curr}</button><button id = "delete"><i class="fa fa-trash"></i></button></li>`
-		);
-	}, '');
-	ScategoyUl.innerHTML = `${Scategorys}<input id='cateogryInput' type='text'></input><button id='categoryAdd'>추가</button>`;
+async function categoryReset(targetCategory) {
+	const category = document.createElement('div');
+	category.classList.add('category-item');
+	console.log(categorysData);
+	categorysData.forEach((data) => {
+		const item = addItem(data);
+		category.append(item);
+	});
+
+	const categoryInput = document.createElement('input');
+	categoryInput.id = 'categoryInput';
+	categoryInput.type = 'text';
+	const categoryAdd = document.createElement('button');
+	categoryAdd.id = categoryAdd;
+	categoryAdd.textContent = '추가';
+
+	ScategoyUl.innerHTML = null;
+	ScategoyUl.append(category, categoryInput, categoryAdd);
+
+	categoryAdd.addEventListener('click', async (e) => {
+		e.preventDefault();
+
+		const ScateogryInput = categoryInput.value;
+		console.log(targetCategory, ScateogryInput);
+		const data = { targetCategory, ScateogryInput };
+		const newCategory = await Api.post('/api/category_update', data);
+		if (newCategory.result == 'ok') {
+			categorysData.push(ScateogryInput);
+			categoryReset(targetCategory, categorysData);
+		} else {
+			alert('error');
+		}
+	});
+
+	
+}
+
+function addItem(itemName) {
+	const li = document.createElement('li');
+	li.id = 'categoryId';
+	const btn = document.createElement('button');
+	btn.id = 'categoryName';
+	btn.textContent = itemName;
+
+	const updateInput = document.createElement('input');
+	updateInput.id = 'updateInput';
+	updateInput.classList.add('hide');
+	const updateInputSubmit = document.createElement('button');
+	updateInputSubmit.id = 'updateInputSubmit';
+	updateInputSubmit.classList.add('hide');
+	updateInputSubmit.textContent = '저장';
+
+	const delBtn = document.createElement('button');
+	delBtn.id = 'delete';
+	const delIcon = document.createElement('i');
+	delIcon.classList.add('fa', 'fa-trash');
+	delBtn.append(delIcon);
+
+	const updateBtn = document.createElement('button');
+	updateBtn.id = 'update';
+	const updateIcon = document.createElement('i');
+	updateIcon.classList.add('fas', 'fa-edit');
+	updateBtn.append(updateIcon);
+
+	li.append(btn, updateInput, updateInputSubmit, delBtn, updateBtn);
+
+	btn.addEventListener('click', (e) => {
+		e.preventDefault();
+		const categoryData = btn.textContent;
+		BcategoryInput.value = getElement('#categorySelectBox').value;
+		ScategoryInput.value = categoryData;
+		document.querySelector('.modal_wrap').style.display = 'none';
+		document.querySelector('.black_bg').style.display = 'none';
+	});
+
+	delBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		try {
+			const selectedCategory = { selectedCategory: itemName };
+			Api.delete('/api/Categorydelete', '', selectedCategory).then(() => {
+				li.remove();
+				categorysData.forEach((data, index) => {
+					if (data === itemName) {
+						categorysData.splice(index, 1);
+					}
+				});
+				console.log(categorysData);
+			});
+		} catch (err) {
+			alert(err.message);
+		}
+	});
+	updateBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		delBtn.classList.toggle('hide');
+		btn.classList.toggle('hide');
+		updateInput.classList.toggle('hide');
+		updateInputSubmit.classList.toggle('hide');
+	});
+	updateInputSubmit.addEventListener('click', (e) => {
+		e.preventDefault();
+		try {
+			const newData = updateInput.value;
+			const data = { oldData: itemName, newData: newData };
+			console.log(data);
+			Api.patch('/api/Ucategory', '', data).then(() => {
+				btn.textContent = newData;
+				categorysData.forEach((data, index) => {
+					categorysData[index] === data
+						? (categorysData[index] = newData)
+						: false;
+				});
+			});
+		} catch (err) {
+			alert(err.message);
+		}
+	});
+	return li;
 }
