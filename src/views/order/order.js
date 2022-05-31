08b53b1a303298925cbe22e65ccc1a0acd072d81
daun3046 <daun3046@swu.ac.kr>
2022-05-31 17:21:27 +0900

    주문정보조회 삭제기능 구현

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -1,10 +1,9 @@
 import { renderGnb } from '/renderGnb.js'
 import * as Api from '/api.js';
+import { getElement, getElementAll } from '/useful-functions.js';
 
-const orderInfoButton = document.querySelector('#orderInfoButton');
-const cancelOrderButton = document.querySelector('#cancelOrderButton');
+const orderTableTitle = getElement('.orderTableTitle');
 
-addAllEvents();
 addAllElements();
 
 function addAllElements() {
@@ -12,39 +11,31 @@ function addAllElements() {
   getOrder();
 }
 
-function addAllEvents() {
-  orderInfoButton.addEventListener("click", orderInfo);
-  cancelOrderButton.addEventListener("click", cancelOrder);
-
-}
-
-async function getOrder(){
-  // const params = location.href.split('/')[4]
-  // console.log(params)
-  // const order = await Api.get('/api/orders',params)
-  // console.log(order)
+async function getOrder() {
+  const orders = await Api.get('/api/orders');
+  console.log(orders);
   
+  orders.forEach((obj) =>
+    orderTableTitle.insertAdjacentHTML('afterend', 
+    `<div class="orderTableRow">
+        <div class="orderTableCell" id="orderDate">${obj["timeKor"]}</div>
+        <div class="orderTableCell" id="orderInfo"><a href="/payment/${obj["orderId"]}">${obj["orderId"]}</a></div>
+        <div class="orderTableCell" id="orderProduct"><a href="/payment/${obj["orderId"]}">${obj["product"]}</a></div>
+        <div class="orderTableCell" id="orderPriceSum">${obj["priceSum"]}원</div>
+        <div class="orderTableCell" id="orderState">${obj["deliveryStatus"]}</div>
+        <button class="cancelOrderButton" name=${obj["orderId"]}>주문취소</button></div>
+      </div>
+  `)
+  )
+  const cancelOrderButtons = getElementAll('.cancelOrderButton');
+  var buttonArray = [...cancelOrderButtons];
+  buttonArray.forEach((btn) => btn.addEventListener("click", cancelOrder));
+  console.log(buttonArray);
 }
 
-function chatFn() {
-  var chatWidth = 450;
-  var chatHeight = 655;
-  var chatLeft = 90;
-  var chatTop = 100;
-  // xPos = (document.body.offsetWidth) - w; // 오른쪽 정렬
-  // xPos += window.screenLeft; // 듀얼 모니터일 때
-  // var yPos = (document.body.offsetHeight/2) - (h/2);
-  window.open('chat', 'a', `width = ${chatWidth}px, height = ${chatHeight}px,left = ${chatLeft}%,top = ${chatTop}`);
-}
-
-async function orderInfo() {
-  //주문 조회 상세 페이지 창 작게 띄움
-  // const result = await Api.post('/api/orderadd',data)
-  // console.log(result);
-  // location.href = `/payment/${result.orderId}`;
-  await chatFn();
-}
-
-function cancelOrder() {
-
+async function cancelOrder() {
+  // 버튼 되면 name과 같은 주문번호 데이터 삭제하는 코드 추가
+  console.log(this.name);
+  const orders = await Api.delete(`/api/orders/${this.name}`);
+  location.reload();
 }
