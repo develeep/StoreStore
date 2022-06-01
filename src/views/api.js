6aaf6cd5602d17a3875c969b79995e51e16d2af8
Lee <dltmdgns0508@gmail.com>
2022-06-02 03:16:37 +0900

    상품 추가, 수정 삭제 기능 구현

--- src/views/api.js
+++ src/views/api.js
@@ -91,6 +91,31 @@ async function formPost(endpoint, formData) {
 
 	return result;
 }
+
+async function formPatch(endpoint,params, formData) {
+	const apiUrl = `${endpoint}/${params}`;
+	// JSON.stringify 함수: Javascript 객체를 JSON 형태로 변환함.
+	// 예시: {name: "Kim"} => {"name": "Kim"}
+	const res = await fetch(apiUrl, {
+		method: 'PATCH',
+		headers: {
+			Authorization: `Bearer ${localStorage.getItem('token')}`,
+		},
+		body: formData,
+	});
+
+	// 응답 코드가 4XX 계열일 때 (400, 403 등)
+	if (!res.ok) {
+		const errorContent = await res.json();
+		const { reason } = errorContent;
+
+		throw new Error(reason);
+	}
+
+	const result = await res.json();
+
+	return result;
+}
 // api 로 PATCH 요청 (/endpoint/params 로, JSON 데이터 형태로 요청함)
 async function patch(endpoint, params = '', data) {
 	const apiUrl = `${endpoint}/${params}`;
@@ -155,4 +180,4 @@ async function del(endpoint, params = '', data = {}) {
 }
 
 // 아래처럼 export하면, import * as Api 로 할 시 Api.get, Api.post 등으로 쓸 수 있음.
-export { get, post, patch, del as delete ,formPost};
+export { get, post, patch, del as delete ,formPost,formPatch};
