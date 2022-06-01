43dafbc55b34e8e1ccb0530c7047dbd10b85f115
[SW2/서울/2반] 연은빈 <okdol0611@gmail.com>
2022-06-01 15:53:04 +0900

    nav 수정 중

--- src/views/admin/admin.js
+++ src/views/admin/admin.js
@@ -1,6 +1,5 @@
-import { renderGnb } from '/renderGnb.js'
+import { renderGnb } from '/renderGnb.js';
 
-addAllElements()
+addAllElements();
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {
-}
+async function addAllElements() {}
